c9d9014d860f1143ef70bc1ba0f9b5aecc393e24
BT_4N2_04 <amejia9149@bths.edu>
2023-05-02 12:29:58 -0400

    added buttons

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("s");
+        MainFrame s= new MainFrame();
+        System.out.println(new MainFrame());
+    }
+}
---
+++ src/MainFrame.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainFrame extends JFrame implements ActionListener {
+    private JPanel Panel;
+    private JButton playButton;
+    private JButton recommendButton;
+    private JButton historyButton;
+    public MainFrame(){
+        this.setVisible(true);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
