491e1769e962f18f169ba677601ba34964dd9130
BT_4N2_04 <amejia9149@bths.edu>
2023-05-10 12:30:33 -0400

    added buttons

--- src/Final.java
+++ src/Final.java
@@ -1,28 +1,37 @@
-import javax.imageio.ImageIO;
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.BlockingDeque;
+import java.io.*;
 
 public class Final{
     private JPanel panel1;
     private JPanel panel2;
     private Image backgroundImage;
+    MediaTracker S;
+    Long currentFrame;
+    Clip clip;
+
 
 
     private JTextArea andySMusicPlayerTextArea;
+    private AudioInputStream audioInputStream;
     private JPasswordField passwordField1;
     private JButton listenButton;
     private JButton recommendButton;
     private JButton playArtistButton;
+    private JList list1;
 
-    public Final() {
+
+    public Final() throws IOException, LineUnavailableException, UnsupportedAudioFileException {
+        audioInputStream= AudioSystem.getAudioInputStream(new File("src/The Weeknd - Blinding Lights (Official Audio).wav").getAbsoluteFile());
+                clip=AudioSystem.getClip();
+        clip.open(audioInputStream);
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+        clip
     }
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
         JFrame frame=new JFrame("App");
         frame.setContentPane(new Final().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -32,6 +41,13 @@ public class Final{
         frame.setLocationRelativeTo(null);
         frame.setSize(540,390);
     }
+    public int actionListener(){
+        if(listenButton.isSelected()){
+            System.out.println("Muy bien");
+        }
+        return -1;
+    }
+
 
 
 }
