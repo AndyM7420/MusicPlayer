0f138ffec99cbaa9e8b44a0e5a22217acf70c24d
andym <amejia9149@bths.edu>
2023-05-12 06:51:49 -0400

    added action listener to pause

--- src/Final.java
+++ src/Final.java
@@ -1,33 +1,64 @@
 import javax.sound.sampled.*;
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
+import java.util.ArrayList;
 
-public class Final{
+public class Final extends JFrame implements ActionListener {
     private JPanel panel1;
     private JPanel panel2;
-    private Image backgroundImage;
-    MediaTracker S;
+    private static Image backgroundImage;
     Long currentFrame;
     Clip clip;
-
-
-
     private JTextArea andySMusicPlayerTextArea;
     private AudioInputStream audioInputStream;
     private JPasswordField passwordField1;
     private JButton listenButton;
     private JButton recommendButton;
     private JButton playArtistButton;
-    private JList list1;
+    private JList<String> list1;
+    DefaultListModel<String> model=new DefaultListModel<>();
+    JLabel label3=new JLabel();
+    JPanel panel=new JPanel();
+    JSplitPane splitPane=new JSplitPane();
+    File songs;
+    String timeStamp;
 
 
     public Final() throws IOException, LineUnavailableException, UnsupportedAudioFileException {
-        audioInputStream= AudioSystem.getAudioInputStream(new File("src/The Weeknd - Blinding Lights (Official Audio).wav").getAbsoluteFile());
-                clip=AudioSystem.getClip();
-        clip.open(audioInputStream);
-        clip.loop(Clip.LOOP_CONTINUOUSLY);
-        clip
+
+        songs=new File("src/The Weeknd - Blinding Lights (Official Audio).wav");
+        audioInputStream= AudioSystem.getAudioInputStream(songs.getAbsoluteFile());
+        clip=AudioSystem.getClip();
+        list1.setModel(model);
+
+        playArtistButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                timeStamp="playing";
+                model.addElement(songs.getName());
+                try {
+                    clip.open(audioInputStream);
+                } catch (LineUnavailableException ex) {
+                    throw new RuntimeException(ex);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+
+            }
+        });
+        recommendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pause();
+            }
+        });
+
     }
 
 
@@ -40,14 +71,16 @@ public class Final{
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
         frame.setSize(540,390);
+
     }
-    public int actionListener(){
-        if(listenButton.isSelected()){
-            System.out.println("Muy bien");
+    public void pause(){
+        if(timeStamp.equals("playing")){
+            clip.stop();
         }
-        return -1;
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
-
+    }
 }
