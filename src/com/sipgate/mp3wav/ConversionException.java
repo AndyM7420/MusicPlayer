5dc132d996f82b6d86a112af7679a843037f039f
andym <amejia9149@bths.edu>
2023-06-02 00:24:19 -0400

    removed mp3 converter

--- src/com/sipgate/mp3wav/ConversionException.java
+++
@@ -1,7 +0,0 @@
-package com.sipgate.mp3wav;
-
-public class ConversionException extends RuntimeException {
-    public ConversionException(Throwable cause) {
-        super("Failed to convert audio data", cause);
-    }
-}
